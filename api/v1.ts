ebb00087ed2a8e75a98b6d29b53089b8207c4dec
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 00:57:40 +0530

    create v1 route

---
+++ api/v1.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const v1 = Router();
+
+v1.post("/orders")
+v1.get("/orders")
+
+export { v1 }
---
+++ app/http/controllers/v1/order.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+const createOrder = (request: Request, response: Response) => {
+
+}
