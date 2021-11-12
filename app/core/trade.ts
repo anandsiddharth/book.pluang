20e82cb3dab879a86c304f8616041119aead4157
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 23:55:29 +0530

    sell function would do the same

--- app/core/trade.ts
+++ app/core/trade.ts
@@ -6,52 +6,6 @@ import { log } from "./log";
 export const trade = async (order: Order) => {
     console.log(`[info] received TYPE:${order.type} SYM:${order.symbol} QTY:${order.quantity} PRICE:${order.price}`)
 
-    if (order.type == "sell") {
-        let orders = await findBuyers(order)
-
-        for (var i = 0; i < orders.length; i++) {
-            if (
-                orders[i].quantity > orders[i].executedQuantity &&
-                order.quantity > order.executedQuantity
-            ) {
-                let canSell = order.quantity - order.executedQuantity
-                let canBuy = orders[i].quantity - orders[i].executedQuantity
-                let quantity = 0;
-                if (canBuy >= canSell) {
-                    quantity = canSell
-                }
-                if (canBuy < canSell) {
-                    quantity = canBuy
-                }
-                orders[i].executedQuantity += quantity;
-                order.executedQuantity += quantity;
-
-                let executePrice = order.price;
-
-                if (orders[i].quantity == orders[i].executedQuantity) {
-                    orders[i].status = "completed"
-                }
-                if (order.quantity == order.executedQuantity) {
-                    order.status = "completed"
-                }
-
-                await OrderModel.updateOne({ _id: order._id }, {
-                    $set: {
-                        executedQuantity: order.executedQuantity,
-                        status: order.status
-                    }
-                })
-                await OrderModel.updateOne({ _id: orders[i]._id }, {
-                    $set: {
-                        executedQuantity: orders[i].executedQuantity,
-                        status: orders[i].status
-                    }
-                })
-
-                log(orders[i], order, quantity, executePrice)
-            }
-        }
-    }
 
     if (order.type == "buy") {
         try {
@@ -73,7 +27,7 @@ export const trade = async (order: Order) => {
                     orders[i].executedQuantity += quantity;
                     order.executedQuantity += quantity;
 
-                    let executePrice = order.price;
+                    let executePrice = orders[i].price;
 
                     if (orders[i].quantity == orders[i].executedQuantity) {
                         orders[i].status = "completed"
