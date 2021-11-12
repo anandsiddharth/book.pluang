96aa66b54e8dc0ea5606d4cfd2bdda2e9bd2e76f
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 09:50:50 +0530

    avg executed price

--- app/models/order.ts
+++ app/models/order.ts
@@ -2,10 +2,12 @@ import { Schema, model, connect } from 'mongoose';
 import { trade } from '../core/trade';
 
 export interface Order {
+    _id: string
     user: number,
     type: 'buy' | 'sell',
     symbol: string
     price: number
+    avereageExecutedPrice: number
     quantity: number
     executedQuantity: number
     status: 'pending' | 'completed'
@@ -19,6 +21,7 @@ const schema = new Schema<Order>({
     type: { type: String, enum: ['buy', 'sell'], default: 'buy', required: true },
     symbol: { type: String, required: true },
     price: { type: Number, required: true },
+    avereageExecutedPrice: { type: Number, default: 0 },
     quantity: { type: Number, required: true, default: 1, min: 1 },
     executedQuantity: { type: Number, default: 0 },
     status: { type: String, required: true, default: 'pending', enum: ['pending', 'completed'] }
