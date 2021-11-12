c2e2247fd0cd76b31e5eb467a5dea401a1b373a1
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 08:38:45 +0530

    findSellers

---
+++ app/core/findSellers.ts
@@ -0,0 +1,34 @@
+import { Order, OrderModel } from "../models/Order";
+
+export const findSellers = async (order: Order): Promise<Order[]> => {
+
+    let orders: Order[] = []
+
+    let quantityCovered = 0;
+    let index = 0;
+    let FindOrder;
+    do {
+        try {
+
+            FindOrder = await OrderModel.findOne({
+                user: { $ne: order.user },
+                type: 'buy',
+                price: { $gt: order.price },
+                quantity: { $gte: order.quantity },
+                status: "pending"
+            }, {}, { sort: { createdAt: -1 }, skip: index })
+
+            if (FindOrder) {
+                index++;
+                quantityCovered += FindOrder.quantity
+                orders.push()
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+
+    } while (FindOrder && quantityCovered == order.quantity)
+
+    return orders;
+}
