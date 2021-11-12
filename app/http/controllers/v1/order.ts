5e165c12c80061ce02d77cc3c662160cbbd788ee
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 07:29:46 +0530

    create Order

--- app/http/controllers/v1/order.ts
+++ app/http/controllers/v1/order.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { OrderModel } from "../../../models/Order";
 
-const createOrder = (request: Request, response: Response) => {
+const createOrder = async (request: Request, response: Response) => {
+    try {
+        let order = await OrderModel.create({ ...request.body, ...{ user: request.headers["user"] } })
+        response.send(order);
+    } catch (er) {
+        response.send(er)
+    }
+}
 
+
+export {
+    createOrder
 }
