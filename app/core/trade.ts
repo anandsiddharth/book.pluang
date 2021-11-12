9d83d567623543565428cd944bafddc238a7355a
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 17:44:23 +0530

    fix trade

--- app/core/trade.ts
+++ app/core/trade.ts
@@ -5,15 +5,14 @@ import { log } from "./log";
 
 export const trade = async (order: Order) => {
     console.log(`[info] received TYPE:${order.type} SYM:${order.symbol} QTY:${order.quantity} PRICE:${order.price}`)
-    console.log(`[info] finding TYPE:${order.type} SYM:${order.symbol} QTY:${order.quantity} PRICE:${order.price}`)
 
     if (order.type == "sell") {
         let orders = await findBuyers(order)
 
         for (var i = 0; i < orders.length; i++) {
             if (
-                orders[i].quantity < orders[i].executedQuantity &&
-                order.quantity < order.executedQuantity
+                orders[i].quantity > orders[i].executedQuantity &&
+                order.quantity > order.executedQuantity
             ) {
                 let canSell = order.quantity - order.executedQuantity
                 let canBuy = orders[i].quantity - orders[i].executedQuantity
@@ -27,7 +26,7 @@ export const trade = async (order: Order) => {
                 orders[i].executedQuantity += quantity;
                 order.executedQuantity += quantity;
 
-                let avgExecutedPrice = (orders[i].price + order.price) / 2
+                let executePrice = order.price;
 
                 if (orders[i].quantity == orders[i].executedQuantity) {
                     orders[i].status = "completed"
@@ -36,49 +35,70 @@ export const trade = async (order: Order) => {
                     order.status = "completed"
                 }
 
-                await OrderModel.updateOne({ _id: order._id }, { $set: order })
-                await OrderModel.updateOne({ _id: orders[i]._id }, { $set: orders })
+                await OrderModel.updateOne({ _id: order._id }, {
+                    $set: {
+                        executedQuantity: order.executedQuantity,
+                        status: order.status
+                    }
+                })
+                await OrderModel.updateOne({ _id: orders[i]._id }, {
+                    $set: {
+                        executedQuantity: orders[i].executedQuantity,
+                        status: orders[i].status
+                    }
+                })
 
-                log(orders[i], order, avgExecutedPrice)
+                log(orders[i], order, quantity, executePrice)
             }
         }
     }
 
     if (order.type == "buy") {
-        let orders = await findSellers(order)
+        try {
+            let orders = await findSellers(order);
+            for (var i = 0; i < orders.length; i++) {
+                if (
+                    orders[i].quantity > orders[i].executedQuantity &&
+                    order.quantity > order.executedQuantity
+                ) {
+                    let canSell = orders[i].quantity - orders[i].executedQuantity
+                    let canBuy = order.quantity - order.executedQuantity
+                    let quantity = 0;
+                    if (canBuy >= canSell) {
+                        quantity = canSell
+                    }
+                    if (canBuy < canSell) {
+                        quantity = canBuy
+                    }
+                    orders[i].executedQuantity += quantity;
+                    order.executedQuantity += quantity;
 
-        for (var i = 0; i < orders.length; i++) {
-            if (
-                orders[i].quantity < orders[i].executedQuantity &&
-                order.quantity < order.executedQuantity
-            ) {
-                let canSell = orders[i].quantity - orders[i].executedQuantity
-                let canBuy = order.quantity - order.executedQuantity
-                let quantity = 0;
-                if (canBuy >= canSell) {
-                    quantity = canSell
-                }
-                if (canBuy < canSell) {
-                    quantity = canBuy
-                }
-                orders[i].executedQuantity += quantity;
-                order.executedQuantity += quantity;
+                    let executePrice = order.price;
 
-                let avgExecutedPrice = (orders[i].price + order.price) / 2
-                orders[i].avereageExecutedPrice = orders[i].avereageExecutedPrice != 0 ? (orders[i].avereageExecutedPrice + avgExecutedPrice) / 2 : avgExecutedPrice
-                order.avereageExecutedPrice = order.avereageExecutedPrice != 0 ? (order.avereageExecutedPrice + avgExecutedPrice) / 2 : avgExecutedPrice
-                if (orders[i].quantity == orders[i].executedQuantity) {
-                    orders[i].status = "completed"
-                }
-                if (order.quantity == order.executedQuantity) {
-                    order.status = "completed"
-                }
+                    if (orders[i].quantity == orders[i].executedQuantity) {
+                        orders[i].status = "completed"
+                    }
+                    if (order.quantity == order.executedQuantity) {
+                        order.status = "completed"
+                    }
+                    await OrderModel.updateOne({ _id: order._id }, {
+                        $set: {
+                            executedQuantity: order.executedQuantity,
+                            status: order.status
+                        }
+                    })
+                    await OrderModel.updateOne({ _id: orders[i]._id }, {
+                        $set: {
+                            executedQuantity: orders[i].executedQuantity,
+                            status: orders[i].status
+                        }
+                    })
 
-                await OrderModel.updateOne({ _id: order._id }, { $set: order })
-                await OrderModel.updateOne({ _id: orders[i]._id }, { $set: orders })
-
-                log(order, orders[i], avgExecutedPrice)
+                    log(order, orders[i], quantity, executePrice)
+                }
             }
+        } catch (e) {
+            console.log(e)
         }
     }
 
