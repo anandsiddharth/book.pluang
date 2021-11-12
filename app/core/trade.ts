2406b712b547adcbce96581a92c5ef4bfa00bb3f
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 08:16:01 +0530

    trade function

--- app/core/trade.ts
+++ app/core/trade.ts
@@ -1,11 +1,19 @@
 import { Order, OrderModel } from "../models/Order";
+import { matchBuyer } from "./matchBuyer";
+import { matchSeller } from "./matchSeller";
 
-export const trade = (order: Order) => {
+export const trade = async (order: Order) => {
     console.log(`[info] received TYPE:${order.type} SYM:${order.symbol} QTY:${order.quantity} PRICE:${order.price}`)
     console.log(`[info] finding TYPE:${order.type} SYM:${order.symbol} QTY:${order.quantity} PRICE:${order.price}`)
 
+    if (order.type == "sell") {
+        let orders = await matchBuyer(order)
+        console.log(`[info] found ${orders.length}`)
+    }
 
-    OrderModel.find()
-
+    if (order.type == "buy") {
+        let orders = matchSeller(order)
+        console.log(`[info] found ${orders.length}`)
+    }
 
 }
