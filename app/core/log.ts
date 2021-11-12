9995525667d776e3d486fef39484dc339d2adddb
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-13 00:24:09 +0530

    write to file

--- app/core/log.ts
+++ app/core/log.ts
@@ -1,5 +1,12 @@
 import { Order } from "../models/order";
-
+import fs from 'fs'
 export const log = (buy: Order, sell: Order, quantity: number, price: number) => {
     console.log(`[log] #${sell.orderId} ${quantity} ${price} #${buy.orderId}`)
+
+
+    let fileName = `${buy.createdAt.getDate()}-${buy.createdAt.getMonth()}-${buy.createdAt.getFullYear()}`
+
+
+    fs.appendFileSync(`logs/` + fileName, `#${sell.orderId} ${quantity} ${price} #${buy.orderId}\n`);
+
 }
