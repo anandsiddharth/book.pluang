a4959fb1120c3d2b3252f1e7bbddef958be90169
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 07:30:07 +0530

    create router

--- api/v1.ts
+++ api/v1.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { createOrder } from '../app/http/controllers/v1/order';
+import { authMiddleware } from '../app/http/middleware/v1/auth';
 
 const v1 = Router();
 
-v1.post("/orders")
+v1.use(authMiddleware)
+
+v1.post("/orders", createOrder)
 v1.get("/orders")
 
 export { v1 }
