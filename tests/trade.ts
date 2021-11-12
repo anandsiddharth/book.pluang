3e5d7b14704a0c96ffad5d8e0a0ecaba3bb9598b
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 23:55:15 +0530

    trade test case done

--- tests/trade.ts
+++ tests/trade.ts
@@ -19,22 +19,22 @@ describe('orders', function () {
 
 
     it("should create sample buy order", async function () {
-        try {
-            await OrderModel.create({
-                user: 3,
-                type: 'sell',
-                quantity: 110,
-                price: 245.10,
-                symbol: 'BTC'
-            });
-        } catch (e) {
-            console.log(e)
-        }
+
+        await OrderModel.create({
+            orderId: 101,
+            user: 3,
+            type: 'sell',
+            quantity: 110,
+            price: 245.10,
+            symbol: 'BTC'
+        });
+
     });
 
 
     it("should create sample sell order", async function () {
         await OrderModel.create({
+            orderId: 202,
             user: 1,
             type: 'sell',
             quantity: 80,
@@ -45,6 +45,7 @@ describe('orders', function () {
     it("should create sample buy order", async function () {
 
         await OrderModel.create({
+            orderId: 303,
             user: 3,
             type: 'buy',
             quantity: 70,
@@ -55,6 +56,7 @@ describe('orders', function () {
     it("should create sample sell order", async function () {
 
         await OrderModel.create({
+            orderId: 501,
             user: 1,
             type: 'sell',
             quantity: 220,
@@ -64,6 +66,7 @@ describe('orders', function () {
     });
     it("should create sample buy order", async function () {
         await OrderModel.create({
+            orderId: 602,
             user: 3,
             type: 'buy',
             quantity: 50,
@@ -73,6 +76,7 @@ describe('orders', function () {
     });
     it("should create sample buy order", async function () {
         await OrderModel.create({
+            orderId: 701,
             user: 3,
             type: 'buy',
             quantity: 10,
@@ -82,8 +86,8 @@ describe('orders', function () {
     });
     it("should create sample sell order", async function () {
         await OrderModel.create({
+            orderId: 801,
             user: 3,
-
             type: 'sell',
             quantity: 20,
             price: 240.10,
@@ -93,8 +97,8 @@ describe('orders', function () {
     it("should create sample buy order", async function () {
 
         await OrderModel.create({
+            orderId: 901,
             user: 3,
-
             type: 'buy',
             quantity: 150,
             price: 242.70,
@@ -102,6 +106,9 @@ describe('orders', function () {
         });
     });
 
-
+    it("should execute the order completely", async function () {
+        let order = await OrderModel.findOne({ orderId: 202 });
+        assert(order?.quantity == order?.executedQuantity)
+    });
 
 });
