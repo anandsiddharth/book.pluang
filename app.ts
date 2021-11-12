063c24a1a96d1fdac1b28bf2935198d6a75bba1a
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 07:30:00 +0530

    main app file

--- app.ts
+++ app.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import { v1 } from './api/v1';
+import { connect } from 'mongoose';
+import bodyParser from 'body-parser'
+import { OrderModel } from './app/models/Order';
 
 const app = express();
 
 const port = process.env.PORT || 8080
 
+let mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/orders"
+
+connect(mongoURI)
+    .then(() => {
+        console.log("[info] connected to database on", mongoURI)
+    }).catch(() => {
+        console.error("[error] unable to connect to database")
+        process.exit(1)
+    })
+
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 
 app.listen(port, () => {
-    console.log(`App listening on ${port}`)
+    console.log(`[info] app listening on ${port}`)
 });
 
 
