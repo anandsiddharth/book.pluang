1dd56390bf73c73a37793cc7537bad69ab0e52f8
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-12 17:44:28 +0530

    fix log

--- app/core/log.ts
+++ app/core/log.ts
@@ -1,5 +1,5 @@
 import { Order } from "../models/order";
 
-export const log = (buy: Order, sell: Order, avgPrice: number) => {
-
+export const log = (buy: Order, sell: Order, quantity: number, price: number) => {
+    console.log(`[log] ${buy._id} ${quantity} ${price} ${sell._id}`)
 }
